43af71df03fb683054e27e4c0b0db2e1a1e525e2
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-01-18 16:15:53 -0700

    test 3

--- A2_Recipe_App/src/recipes/urls.py
+++ A2_Recipe_App/src/recipes/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from recipes.views import recipes_home  
 
+# test 3
 urlpatterns = [
     path('', recipes_home, name='home'),
     
