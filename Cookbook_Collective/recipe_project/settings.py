b643e72c5ecb55f9dcf7f4fed69eb56eb35d1fd7
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-15 10:39:20 -0700

    becomes cookbook collective

--- Cookbook_Collective/recipe_project/settings.py
+++ Cookbook_Collective/recipe_project/settings.py
@@ -23,11 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pe5n06w4sbre$l)_4@d6$!(fwj+ab33wnzk$ng4o64kj_ri54f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
