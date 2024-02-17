5f30fa415abb72cb196977f3c362f31965450f73
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-17 10:24:01 -0700

    Update DATABASES configuration

--- Cookbook_Collective/recipe_project/settings.py
+++ Cookbook_Collective/recipe_project/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,  # Adjust as needed
+    )
 }
 
 
