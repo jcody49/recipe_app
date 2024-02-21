d2d0bf95de1403ec2c494e273c22711359873a3e
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-20 17:16:29 -0700

    navbar is good on all pages, getting credits to display properly

--- Cookbook_Collective/recipes/views.py
+++ Cookbook_Collective/recipes/views.py
@@ -191,7 +191,7 @@ def credits(request):
     context = {
         'credits': CREDITS,
     }
-    return render(request, 'recipes/credits.html', context)
+    return render(request, 'credits.html', context)
 
 
 class RecipeListView(LoginRequiredMixin, ListView):           #class-based “protected” view
