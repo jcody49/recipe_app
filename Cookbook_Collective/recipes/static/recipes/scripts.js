6b956ee6cbf14d948e236b98782cdd2e3f65ca2b
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-22 00:14:40 -0700

    fixing up navbar in auth files

--- Cookbook_Collective/recipes/static/recipes/scripts.js
+++ Cookbook_Collective/recipes/static/recipes/scripts.js
@@ -2,21 +2,22 @@ console.log('Script loaded!');
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM content loaded!');
     // slick-carousel script
-    $('.scrolling-images').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 100,
-        speed: 10000,
-        pauseOnHover: false,
-    });
+    //$('.scrolling-images').slick({
+        //infinite: true,
+        //slidesToShow: 3,
+        //slidesToScroll: 1,
+        //autoplay: true,
+        //autoplaySpeed: 100,
+      //  speed: 10000,
+    //    pauseOnHover: false,
+    //});
 
     // NAVBAR
     var navbarButton = document.querySelector('.navbar-toggler');
     var navbarCollapse = document.getElementById('navbarNav');
 
     navbarButton.addEventListener('click', function () {
+        console.log("clicked")
         var expanded = navbarButton.getAttribute('aria-expanded') === 'true';
         navbarButton.setAttribute('aria-expanded', String(!expanded));
         navbarCollapse.classList.toggle('show', !expanded);
