01a49ed1cac2bb89ca3405e4abe23232bfe46f9c
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-22 00:05:54 -0700

    fixing up navbar in auth files

--- Cookbook_Collective/recipe_project/urls.py
+++ Cookbook_Collective/recipe_project/urls.py
@@ -1,4 +1,4 @@
-#test
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
--- Cookbook_Collective/recipes/static/recipes/scripts.js
+++ Cookbook_Collective/recipes/static/recipes/scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM content loaded!');
     // slick-carousel script
     $('.scrolling-images').slick({
         infinite: true,
