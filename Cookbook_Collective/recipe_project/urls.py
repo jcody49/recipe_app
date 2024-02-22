a31f5b9c05ab156644606e0462310c2d5c97d70d
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-21 23:43:06 -0700

    fixing up navbar in auth files

--- Cookbook_Collective/recipe_project/urls.py
+++ Cookbook_Collective/recipe_project/urls.py
@@ -1,5 +1,6 @@
 #test
 from django.contrib import admin
+from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,13 +9,13 @@ from .views import login_view, logout_view, signup, delete_account
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', recipes_home, name='home'),  # Separate path for the 'home' view
-    path('recipes/', include('recipes.urls')),  # Include recipes app URLs
+    path('', recipes_home, name='home'),
+    path('recipes/', include('recipes.urls')),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup, name='signup'),
     path('delete_account/', delete_account, name='delete_account'),
 ]
 
-# Specifies the URL “/media/” that will trigger this media view (in this case, an image)
+# specifies the URL “/media/” that will trigger this media view (in this case, an image)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
