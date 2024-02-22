6b4867856c9ec4e119d55c0c38f3b0ccc925cff0
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-21 17:32:07 -0700

    no more style or script tags; paginating search results

--- Cookbook_Collective/recipes/static/recipes/scripts.js
+++ Cookbook_Collective/recipes/static/recipes/scripts.js
@@ -1,31 +1,30 @@
-
-/*slick-carousel script*/
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
+    // slick-carousel script
     $('.scrolling-images').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1, // Change this to 1
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 100,
-        speed: 10000, // Adjust the speed for a smoother continuous scroll
-        pauseOnHover: false, // Allow continuous scroll even on hover
+        speed: 10000,
+        pauseOnHover: false,
     });
-});
 
-//NAVBAR
-document.addEventListener('DOMContentLoaded', function () {
+    // NAVBAR
     var navbarButton = document.querySelector('.navbar-toggler');
     var navbarCollapse = document.getElementById('navbarNav');
 
     navbarButton.addEventListener('click', function () {
         var expanded = navbarButton.getAttribute('aria-expanded') === 'true';
         navbarButton.setAttribute('aria-expanded', !expanded);
-        navbarCollapse.classList.toggle('show'); // If you want to toggle the 'show' class as well
+        navbarCollapse.classList.toggle('show');
     });
-});
-   
 
-//LOADING SPINNER  
-document.getElementById('submitBtn').addEventListener('click', function() {
-    document.getElementById('loading-spinner').style.display = 'block';
+    // LOADING SPINNER
+    var submitBtn = document.getElementById('submitBtn');
+    if (submitBtn) {
+        submitBtn.addEventListener('click', function() {
+            document.getElementById('loading-spinner').style.display = 'block';
+        });
+    }
 });
