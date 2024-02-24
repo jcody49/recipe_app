08f3a4cd1c500760cb9d4775c99575cdaadaf1b7
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-02-24 16:56:27 -0700

    revert_delete_account_view

--- Cookbook_Collective/recipe_project/views.py
+++ Cookbook_Collective/recipe_project/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
